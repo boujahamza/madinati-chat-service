748ca90f9a6e4eb7ff896c9d8dea0c4d0a4836f8
boujahamza <73479328+boujahamza@users.noreply.github.com>
2022-01-28 22:57:47 +0100

    Add files via upload

--- chatService.js
+++ chatService.js
@@ -1,18 +1,29 @@
-const chatService = require('express')()
-const http = require('http').createServer(chatService)
+const httpServer = require('http').createServer()
+const socketIO = require('socket.io')(httpServer)
 
+socketIO.on('connection', function (client) {
+  console.log('Connected...', client.id);
 
-chatService.get('/', (req, res) => {
-    res.send("Node Server is running. Yay!!")
+//listens for new messages coming in
+  client.on('message', function name(data) {
+    console.log(data);
+    socketIO.emit('message', data);
+  })
+
+//listens when a user is disconnected from the server
+  client.on('disconnect', function () {
+    console.log('Disconnected...', client.id);
+  })
+
+//listens when there's an error detected and logs the error on the console
+  client.on('error', function (err) {
+    console.log('Error detected', client.id);
+    console.log(err);
+  })
 })
 
-//Socket Logic
-const socketio = require('socket.io')(http)
-
-socketio.on("connection", (userSocket) => {
-    userSocket.on("send_message", (data) => {
-        userSocket.broadcast.emit("receive_message", data)
-    })
-})
-
-http.listen(process.env.PORT)
+var port = process.env.PORT || 3000;
+httpServer.listen(port, function (err) {
+  if (err) console.log(err);
+  console.log('Listening on port', port);
+});
